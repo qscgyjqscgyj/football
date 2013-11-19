0dd760aa9f868bbb9cc11c735b0fdeb101c64c36
Aleksandr <greenteamer@bk.ru>
2013-11-19 19:54:31 +0700

    Главная страница почти готова

--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.views.generic import TemplateView
 from slider.models import Slider
+from game.models import Game
 
 
 class MainView(TemplateView):
@@ -9,4 +10,5 @@ class MainView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(MainView, self).get_context_data()
         context['slides'] = Slider.objects.all()
+        context['games'] = Game.objects.all()
         return context
