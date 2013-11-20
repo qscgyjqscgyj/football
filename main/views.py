d1434980a48783a824c9c6fd0a0dd212bdb37a4c
mike <aliestarten@gmail.com>
2013-11-20 18:37:27 +0800

    add supernumerary on base

--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.views.generic import TemplateView
 from slider.models import Slider
+from user_profile.models import Supernumerary
 
 
 class MainView(TemplateView):
@@ -9,4 +10,5 @@ class MainView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(MainView, self).get_context_data()
         context['slides'] = Slider.objects.all()
+        context['supernumeraries'] = Supernumerary.objects.all()
         return context
