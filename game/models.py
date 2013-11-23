482730cf7827079b50f08c6df1ad5fbb4ccbca5b
mike <aliestarten@gmail.com>
2013-11-23 23:34:57 +0800

    add forecast add & check supernumerary answers

--- game/models.py
+++ game/models.py
@@ -2,6 +2,7 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from user_profile.models import Supernumerary
+from django.db.models import F
 
 
 class Team(models.Model):
@@ -26,6 +27,23 @@ class SportType(models.Model):
         verbose_name_plural = _(u'Виды спорта')
 
 
+class Forecast(models.Model):
+    supernumerary = models.ForeignKey(Supernumerary, verbose_name=_(u'Статист'),
+                                      related_name='forecast_supernumerary', blank=True, null=True)
+    game = models.ForeignKey('Game', verbose_name=_(u'Игра'), related_name='forecast_game')
+    score = models.CharField(verbose_name=_(u'Счет'), max_length=100, blank=True, null=True)
+    draw = models.BooleanField(verbose_name=_(u'Ничья'))
+    win_team = models.ForeignKey(Team, verbose_name=_(u'Победившая команда'), related_name='forecast_team',
+                                 blank=True, null=True)
+
+    def __unicode__(self):
+        return self.game.__unicode__()
+
+    class Meta:
+        verbose_name = _(u'Прогноз')
+        verbose_name_plural = _(u'Прогнозы')
+
+
 class Game(models.Model):
     sport = models.ForeignKey(SportType, verbose_name=_(u'Вид спорта'), related_name='game_sport_type',
                               blank=True, null=True)
@@ -41,23 +59,36 @@ class Game(models.Model):
         return (unicode(self.sport) + " " + self.team1 +
                 " vs " + self.team2 + " " + str(self.date))
 
+    def save(self, *args, **kwargs):
+        if self.score or self.draw or self.win_team:
+            forecasts = Forecast.objects.filter(game=self)
+            for forecast in forecasts:
+                if forecast.score and forecast.draw and forecast.win_team:
+                    supernumerary = Supernumerary.objects.get(id=forecast.supernumerary.pk)
+                    supernumerary.wrong = F('wrong') + 1
+                    supernumerary.save()
+                elif forecast.score and forecast.draw:
+                    supernumerary = Supernumerary.objects.get(id=forecast.supernumerary.pk)
+                    supernumerary.wrong = F('wrong') + 1
+                    supernumerary.save()
+                elif forecast.draw and forecast.win_team:
+                    supernumerary = Supernumerary.objects.get(id=forecast.supernumerary.pk)
+                    supernumerary.wrong = F('wrong') + 1
+                    supernumerary.save()
+                elif forecast.score and forecast.win_team:
+                    supernumerary = Supernumerary.objects.get(id=forecast.supernumerary.pk)
+                    supernumerary.wrong = F('wrong') + 1
+                    supernumerary.save()
+                elif (forecast.score == self.score and forecast.score) or (forecast.draw == self.draw and forecast.draw) or (forecast.win_team == self.win_team and forecast.win_team):
+                    supernumerary = Supernumerary.objects.get(id=forecast.supernumerary.pk)
+                    supernumerary.right = F('right') + 1
+                    supernumerary.save()
+                else:
+                    supernumerary = Supernumerary.objects.get(id=forecast.supernumerary.pk)
+                    supernumerary.wrong = F('wrong') + 1
+                    supernumerary.save()
+        super(Game, self).save(*args, **kwargs)
+
     class Meta:
         verbose_name = _(u'Игра')
         verbose_name_plural = _(u'Игры')
-
-
-class Forecast(models.Model):
-    supernumerary = models.ForeignKey(Supernumerary, verbose_name=_(u'Статист'),
-                                      related_name='forecast_supernumerary')
-    game = models.ForeignKey(Game, verbose_name=_(u'Игра'), related_name='forecast_game')
-    score = models.CharField(verbose_name=_(u'Счет'), max_length=100, blank=True, null=True)
-    draw = models.BooleanField(verbose_name=_(u'Ничья'))
-    win_team = models.ForeignKey(Team, verbose_name=_(u'Победившая команда'), related_name='forecast_team',
-                                 blank=True, null=True)
-
-    def __unicode__(self):
-        return self.game.__unicode__()
-
-    class Meta:
-        verbose_name = _(u'Прогноз')
-        verbose_name_plural = _(u'Прогнозы')
--- game/views.py
+++ game/views.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 import json
-from django.views.generic import ListView, DetailView
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import HttpResponseRedirect
+from django.views.generic import ListView, DetailView, FormView
+from game.forms import ForecastForm
 from game.models import *
 import datetime
+from user_profile.models import CustomUser
 
 
 class GameListView(ListView):
@@ -32,3 +36,45 @@ class GameDetailView(DetailView):
     template_name = "game.html"
     context_object_name = "game"
     pk_url_kwarg = "pk"
+
+
+class NewForecastView(FormView):
+    template_name = 'forecast.html'
+    success_url = '/forecast/new'
+    form_class = ForecastForm
+
+    def get_context_data(self, **kwargs):
+        context = super(NewForecastView, self).get_context_data(**kwargs)
+        context['forecasts'] = Forecast.objects.all()
+        user = self.request.user
+        try:
+            custom_user = CustomUser.objects.get(pk=user.pk).pk
+        except ObjectDoesNotExist:
+            custom_user = False
+        if user.pk == custom_user:
+            context['custom_user'] = True
+        return context
+
+    def form_valid(self, form_class):
+        if self.request.method == 'POST':
+            if form_class.is_valid():
+                message = form_class.save()
+                message.supernumerary = self.request.user.supernumerary
+                message.save()
+            return HttpResponseRedirect(self.get_success_url())
+
+    def get_form_kwargs(self):
+        kwargs = super(NewForecastView, self).get_form_kwargs()
+        games = []
+        all_games = Game.objects.all()
+        try:
+            supernumerary = self.request.user.supernumerary
+            forecasts = Forecast.objects.filter(supernumerary=supernumerary)
+            for game in all_games:
+                for forecast in forecasts:
+                    if game == forecast.game:
+                        games.append(game.pk)
+            self.form_class.base_fields['game'].queryset = Game.objects.exclude(id__in=games)
+            return kwargs
+        except ObjectDoesNotExist:
+            return kwargs
