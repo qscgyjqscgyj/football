12881611c0df916b67e4aff0629be06c13dce7f9
mike <aliestarten@gmail.com>
2013-12-05 14:50:10 +0800

    done supernumerary admin form & done checking forecast right and wrong

--- game/admin.py
+++ game/admin.py
@@ -3,7 +3,12 @@ from django.contrib import admin
 from game.models import *
 
 
+class ForecastAdmin(admin.ModelAdmin):
+    ordering = ['supernumerary']
+    list_display = (Forecast.__unicode__, 'right', 'supernumerary', )
+
+
 admin.site.register(Game)
 admin.site.register(Team)
 admin.site.register(SportType)
-admin.site.register(Forecast)
+admin.site.register(Forecast, ForecastAdmin)
--- user_profile/admin.py
+++ user_profile/admin.py
@@ -1,13 +1,24 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserChangeForm
 from user_profile.models import *
 
 
-class CustomAdmin(UserAdmin):
-    list_display = ('username', 'email', 'is_staff', 'is_active', 'fio', 'photo',)
-    list_filter = ('is_staff', 'is_superuser', 'is_active', )
+class SupernumeraryChangeForm(UserChangeForm):
 
-admin.site.register(Supernumerary)
+    class Meta:
+        model = Supernumerary
+
+
+class CustomAdmin(UserAdmin):
+    list_display = ('username', 'email', 'is_active', 'fio', )
+    form = SupernumeraryChangeForm
+
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('fio', 'photo', 'right', 'wrong', 'about')}),
+    )
+
+admin.site.register(Supernumerary, CustomAdmin)
 admin.site.register(CustomUser)
 
